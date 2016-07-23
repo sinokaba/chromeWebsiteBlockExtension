32642a2a3c48b27dc125411cf0e55162fe43c5ed
sinokaba <ronaldal.santos@gmail.com>
2016-07-23 19:34:13 -0400

    Add files via upload
    stored blocked sites list in local storage, and made them visible in the popup

--- popup.js
+++ popup.js
@@ -3,27 +3,54 @@ window.onload = function(){
 var obj = {};
 var storage = chrome.storage.local;
 var website;
-
+var getBG = chrome.extension.getBackgroundPage();
+var err;
+var sitesBlocked = 0;
 document.getElementById("blockNow").onclick = function(){
   website = document.getElementById("blockSite").value;
   console.log(website);
-  chrome.extension.getBackgroundPage().enableBlocking(website);
+  getBG.enableBlocking(website);
   
-  document.getElementById("unblockNow").value = "Unblock " + website;
+  setKeys();
 
+  getKeys(); 
+
+  sitesBlocked++;
+}
+
+document.getElementById("unblockNow").value = "Unblock All";
+
+
+
+document.getElementById("unblockNow").onclick = function(){
+  getBG.disableBlocking(website);
+  storage.clear(function(){
+    err = chrome.runtime.lastError;
+    if(err){
+      alert("An error occured, could not remove item.");
+    }
+  }); 
+  getKeys();
+}
+
+function getKeys(){
+  storage.get(null, function(items){
+    var allkeys = Object.keys(items);
+    console.log(allkeys);
+    for(i = 0; i < allkeys.length; i++){
+      document.getElementById("p" + i).innerHTML = allkeys[i];
+      console.log(allkeys[allkeys.length - i]);
+    }
+  });
+}
+
+function setKeys(){
   obj[website] = website;
   storage.set(obj, function(){
      if (chrome.extension.lastError) {
             alert('An error occurred: ' + chrome.extension.lastError.message);
         }
   });
-
-  storage.get(website, function(result){
-    document.getElementById("firstSite").innerHTML = JSON.stringify(result);
-  });
-}
-document.getElementById("unblockNow").onclick = function(){
-  chrome.extension.getBackgroundPage().disableBlocking(website);
 }
 var url = document.getElementById("blockSite");
 url.focus();
