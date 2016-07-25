c5193cc3b1a25e2b8347a6d19b94d1b338c8b494
sinokaba <ronaldal.santos@gmail.com>
2016-07-25 19:25:50 -0400

    fixed blocked site list not showing when pop close

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ var callback = function(details){
 		alert("Cancelling: " + details.url + ". Get back to work!");
 	  	return {cancel: true};
 }
+
 function enableBlocking(site){
 	chrome.webRequest.onBeforeRequest.addListener(callback,
 	{urls: ["*://www." + site + "/*"]},
@@ -10,3 +11,5 @@ function enableBlocking(site){
 function disableBlocking(site){
 	chrome.webRequest.onBeforeRequest.removeListener(callback);	
 }
+
+
--- popup.js
+++ popup.js
@@ -5,18 +5,36 @@ var storage = chrome.storage.local;
 var website;
 var getBG = chrome.extension.getBackgroundPage();
 var err;
-var sitesBlocked = 0;
-document.getElementById("blockNow").onclick = function(){
-  website = document.getElementById("blockSite").value;
+var sitesBlocked = -1;
+var textbox = document.getElementById("blockSite");
+var n = -1;
+function block(n){
+  website = textbox.value;
   console.log(website);
   getBG.enableBlocking(website);
   
-  setKeys();
-
-  getKeys(); 
-
-  sitesBlocked++;
+  if(website != ""){
+    sitesBlocked = n;
+    setKeys();
+    addtoList(website.length + 2); 
+  }
+  else{
+    alert("You did not enter a valid website!");
+  }
 }
+
+textbox.addEventListener("keydown", function(event) {
+  if (event.keyCode == 13) {
+    n++;
+    block(n);
+  }
+})
+
+document.getElementById("blockNow").onclick = function(){
+  n++;
+  block(n);
+}
+
 
 document.getElementById("unblockNow").value = "Unblock All";
 
@@ -29,20 +47,38 @@ document.getElementById("unblockNow").onclick = function(){
     if(err){
       alert("An error occured, could not remove item.");
     }
-  }); 
+  });
+  chrome.runtime.reload();
 }
-getKeys();
+
+if(chrome.extension.getViews({ type: "popup" }) != '[]'){
+  getKeys();
+
+}
+
 function getKeys(){
   storage.get(null, function(items){
     var allkeys = Object.keys(items);
     console.log(allkeys);
     for(i = 0; i < allkeys.length; i++){
-      document.getElementById("p" + i).innerHTML = allkeys[i];
-      console.log(allkeys[allkeys.length - i]);
+    var li = document.createElement('li');
+    li.setAttribute('id', 'site' + i);
+    document.getElementById("blockList").appendChild(li)
+    document.getElementById("site" + i).innerHTML = allkeys[i];
     }
   });
 }
 
+
+function addtoList(len){
+  storage.get(website, function(result){
+    var res = JSON.stringify(result).substring(2, len);
+    var li = document.createElement('li');
+    li.setAttribute('id', 'site' + sitesBlocked);
+    document.getElementById("blockList").appendChild(li)
+    document.getElementById("site" + sitesBlocked).innerHTML = res;
+  });
+}
 function setKeys(){
   obj[website] = website;
   storage.set(obj, function(){
@@ -54,4 +90,3 @@ function setKeys(){
 var url = document.getElementById("blockSite");
 url.focus();
 }
-
