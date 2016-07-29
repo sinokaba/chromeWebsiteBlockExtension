b5dfe21bb4ab1368bafb4b36a7082f68cbba808d
sinokaba <ronaldal.santos@gmail.com>
2016-07-28 20:18:43 -0400

    added ability to unblock each site seperately

--- background.js
+++ background.js
@@ -1,15 +1,37 @@
 var callback = function(details){
-		alert("Cancelling: " + details.url + ". Get back to work!");
-	  	return {cancel: true};
+	alert("Cancelling: " + details.url + ". Get back to work!");
+	console.log(details.timeStamp);
+	return {cancel: true};
+}
+var callback2 = function(details){
+	alert("Cancelling: " + details.url + ". Get back to work!");
+	console.log(details.timeStamp);
+	return {cancel: true};
+}
+function enableBlocking(site, n){
+	if(n == 0){
+		chrome.webRequest.onBeforeRequest.addListener(callback,
+		{urls: ["*://www." + site + "/*"]},
+		["blocking"]);
+	}
+	else if(n == 1){
+		chrome.webRequest.onBeforeRequest.addListener(callback2,
+		{urls: ["*://www." + site + "/*"]},
+		["blocking"]);		
+	}
 }
 
-function enableBlocking(site){
-	chrome.webRequest.onBeforeRequest.addListener(callback,
-	{urls: ["*://www." + site + "/*"]},
-	["blocking"]);
-}
-function disableBlocking(){
-	chrome.webRequest.onBeforeRequest.removeListener(callback);	
+function disableBlocking(n){
+	if(n == 0){
+		chrome.webRequest.onBeforeRequest.removeListener(callback,
+		{urls: ["*://www.reddit.com/*"]},
+		["blocking"]);	
+	}
+	else if(n == 1){
+		chrome.webRequest.onBeforeRequest.removeListener(callback2,
+		{urls: ["*://www.reddit.com/*"]},
+		["blocking"]);	
+	}
 }
 
 
