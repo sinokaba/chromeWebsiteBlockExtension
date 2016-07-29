67ff789f3511c4af4b53dbddbdcd7197acc1a495
sinokaba <ronaldal.santos@gmail.com>
2016-07-28 20:18:09 -0400

    added custom unblock buttons

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-
+//all the global vars i'm using
 var obj = {};
 var permObj = {};
 var local = chrome.storage.local;
@@ -12,10 +12,13 @@ var buttonID = document.getElementById("blockNow");
 var unBlock = document.getElementById("unblockNow");
 var warning = false;
 var confirmed = false;
-
+var n = -1;
+//the dialog for the warning message that appears when the user decides to permanently block a site
 $("#warning-popup").dialog({
   autoOpen: false,
   draggable: false,
+
+  //will disable the background when the popup is opened, so nothing can be interacted with except the popup
   open: function(event, ui) {
     $("#status").css("opacity", ".7");
     document.getElementById("blockNow").disabled = true;
@@ -63,12 +66,16 @@ $("#warning-popup").dialog({
     ]
 });
 
+//listens to the block button, and blocks the website entered into the input field once it's pressed
 buttonID.addEventListener('click', function(){
   website = textbox.value;
-  getBG.enableBlocking(website);
+  n++;
+  document.cookie = n;
+  getBG.enableBlocking(website, document.cookie);
   
+  //regex expression to check whether the user inputted an empty string into the input field
   if(!(/\s/).test(website) && website != ""){
-    tempSetKeys();
+    tempSetKeys(n);
     tempGetKeys();
   }
   else{
@@ -76,6 +83,7 @@ buttonID.addEventListener('click', function(){
   }
 });
 
+//event listener for the permaban button, opens the popup to double check with user before banning
 document.getElementById("permaban").addEventListener('click', function(){
   warning = true;
   website = textbox.value;
@@ -84,20 +92,18 @@ document.getElementById("permaban").addEventListener('click', function(){
   $("#warning-popup").dialog("open");
 });
 
+//triggers the click event when the user presses enter when entering a site
 textbox.addEventListener("keydown", function(event) {
   if (event.keyCode == 13) {
     buttonID.click();
   }
 });
 
-
-
-unBlock.value = "Unblock All";
-
-
-
+//unblocks everthing when the unblock all button is clicked
 unBlock.onclick = function(){
-  getBG.disableBlocking();
+  getBG.disableBlocking(n);
+
+  //clears both local and sync chrome storage systems
   local.clear(function(){
     err = chrome.runtime.lastError;
     if(err){
@@ -105,37 +111,53 @@ unBlock.onclick = function(){
     }
   });
   sync.clear();
+  //reloads the extension, closes the popup
   chrome.runtime.reload();
 }
 
+$(".unblock-button").click(function(){
+  var id = $(this).attr('id');
+  var parseId = id.substring(id.length - 1, id.length);
+  console.log(parseId);
+  getBG.disableBlocking(parseId);
+  removeFromList(parseId);
+
+})
+//checks whether popup is open or not
 if(chrome.extension.getViews({ type: "popup" }) != '[]'){
   tempGetKeys();
   permGetKeys();
 }
-
-function tempGetKeys(){
-    local.get(null, function(items){
-      var allkeys = Object.keys(items);
-      console.log(allkeys);
-      for(i = 0; i < allkeys.length; i++){
-      var li = document.createElement('li');
-      li.setAttribute('id', 'site' + i);
-      document.getElementById("blockList").appendChild(li)
-      document.getElementById("site" + i).innerHTML = allkeys[i];
-      }
-    });
-}
-
-function tempSetKeys(){
-    obj[website] = website;
+//this function to store keys and their respected values to chrome local storage
+function tempSetKeys(k){
+    obj['website' + k] = website;
     local.set(obj, function(){
        if (chrome.extension.lastError) {
               alert("An error occurred: " + chrome.extension.lastError.message);
           }
     });
 }
+//grabbing the keys and their values and creating <li> elements to store them in
+function tempGetKeys(){
+    local.get(null, function(items){
+      var allkeys = Object.keys(items);
+      console.log(items);
+      for(i = 0; i < allkeys.length; i++){
+        var key = allkeys[i];
+        $("#site" + i).text(items[key]);
+        $("#unblock-" + i).removeClass("hide");
+      }
+    });
+}
 
+function removeFromList(n){
+  local.remove('website'+n, function(items){
+    console.log("Website removed");
+  })
+}
+//this function to store keys and their respected values to chrome sync storage
 function permSetKeys(){
+  //stores the website value to permobj variable
   permObj[website] = website;
   sync.set(permObj, function(){
     if(chrome.extension.lastError){
