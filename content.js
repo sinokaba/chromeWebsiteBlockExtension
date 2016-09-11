76f414241e1f46cc45005a48a183d6f202b0969d
Sinokaba <ronaldal.santos@gmail.com>
2016-09-10 21:37:27 -0400

    removed unused stuff, made a little note

--- content.js
+++ content.js
@@ -1,17 +1,5 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-  if(request.act == "getUnblockDate"){
-    if(request.unblockDate == 'na'){
-      document.getElementById("unblockTimer-" + request.sId).innerHTML = "N/A";
-    }
-    else{
-      document.getElementById("unblockTimer-" + request.sId).innerHTML  = request.unblockDate;
-    }
-  }
-  else{
-    console.log("not workong eh?" + " request: " + request.act);
-  }
-});
-document.getElementById("unblockTimer-0").innerHTML = "ok";
+//will change the dom of the website that's blocked be a blank page with theh reason that the user inputted in large black letters
+
 
 function changeBlockedSite(){
   chrome.tabs.insertCSS(null, {file:"inject.css"})
